bc6f07c71a3e7ea6fed535c2291d09723b4d9102
BIZONUSERYAKAMO <2860483411@qq.com>
2025-02-20 18:38:43 +0800

    Revert "gradle大爹我错了 你给我构建吧 我求你了"
    This reverts commit d5d58fb8158e04cb6aa9680e97322ed4e7923ccb.

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/entity/MixinInventoryPlayer.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/entity/MixinInventoryPlayer.java
@@ -7,6 +7,7 @@ package net.ccbluex.liquidbounce.injection.forge.mixins.entity;
 
 import net.ccbluex.liquidbounce.utils.SilentHotbar;
 import net.minecraft.entity.player.InventoryPlayer;
+import org.spongepowered.asm.lib.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -16,7 +17,7 @@ import static net.ccbluex.liquidbounce.utils.MinecraftInstance.mc;
 @Mixin(InventoryPlayer.class)
 public class MixinInventoryPlayer {
 
-    @Redirect(method = {"getCurrentItem", "decrementAnimations", "getStrVsBlock", "canHeldItemHarvest"}, at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/InventoryPlayer;currentItem:I", opcode = 180))
+    @Redirect(method = {"getCurrentItem", "decrementAnimations", "getStrVsBlock", "canHeldItemHarvest"}, at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/InventoryPlayer;currentItem:I", opcode = Opcodes.GETFIELD))
     private int hookSilentHotbar(InventoryPlayer instance) {
         return instance.player.getGameProfile() == mc.thePlayer.getGameProfile() ? SilentHotbar.INSTANCE.getCurrentSlot() : instance.currentItem;
     }
