bba1c5c80300587e13a02ddcd447d8a013f73a7f
BIZONUSERYAKAMO <kamisatoayakamo@yandex.com>
2025-05-08 13:40:04 +0800

    哈基米胖宝宝

--- src/main/java/net/ccbluex/liquidbounce/event/EventManager.kt
+++ src/main/java/net/ccbluex/liquidbounce/event/EventManager.kt
@@ -1,7 +1,7 @@
 /*
- * LiquidBounce Hacked Client
+ * GoldBounce Hacked Client
  * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
- * https://github.com/CCBlueX/LiquidBounce/
+ * https://github.com/bzym2/GoldBounce/
  */
 package net.ccbluex.liquidbounce.event
 import net.ccbluex.liquidbounce.utils.ClientUtils.LOGGER
