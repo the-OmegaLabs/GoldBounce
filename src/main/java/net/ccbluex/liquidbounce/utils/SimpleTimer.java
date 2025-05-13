e6e8415f9c1856f9919033a1d357c8cf5bbfb50e
BIZONUSERYAKAMO <kamisatoayakamo@yandex.com>
2025-05-13 11:22:40 +0800

    Create SimpleTimer.java

---
+++ src/main/java/net/ccbluex/liquidbounce/utils/SimpleTimer.java
@@ -0,0 +1,10 @@
+package net.ccbluex.liquidbounce.utils;
+
+public class SimpleTimer {
+
+    public static float timerSpeed = 0f;
+
+    public static void reset() {
+        timerSpeed = 0f;
+    }
+}
